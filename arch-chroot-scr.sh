17cc875c1a9b66b7843ec98db80cf4ecc89ff5ae
ErrorAI <72821611+ErrorAI@users.noreply.github.com>
2021-09-26 18:54:03 +0400

    Update arch-chroot-scr.sh

--- arch-chroot-scr.sh
+++ arch-chroot-scr.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-echo "yeeHost" > /etc/hostname
+echo "yeeHost" > /etc/hostname    #записать имя хоста
 
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen   
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
+locale-gen         #генерируем локали
 
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf      #выставляем язык системы
 
-echo "KEYMAP=ru" >> /etc/vconsole.conf
-echo "FONT=cyr-sun16" >> /etc/vconsole.conf
+echo "KEYMAP=ru" >> /etc/vconsole.conf          #настройки отображения в консоли
+echo "FONT=cyr-sun16" >> /etc/vconsole.conf     
 
 mkinitcpio -p linux
 
-grub-install /dev/sda
+grub-install /dev/sda                 #установка grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Print root password:"
@@ -25,7 +25,7 @@ passwd user1
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 pacman -Syy
-
+pacman -S vim --noconfirm
 pacman -S xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils --noconfirm
 
 pacman -S xfce4 xfce4-goodeis
