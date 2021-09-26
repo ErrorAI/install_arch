fc27a739f237847e92ce8abf32b54116e79ef57b
ErrorAI <72821611+ErrorAI@users.noreply.github.com>
2021-09-26 18:08:38 +0400

    Update scr.sh

--- scr.sh
+++ scr.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
-sfdisk -l             &&
+
+#создание gpt таблицы и разделов
+(
+  echo g; #создать gpt таблицу
+
+  echo n; #новый раздел (разметим boot)
+  echo;
+  echo;
+  echo +1G;
+
+  echo n; #новый раздел (разметим swap)
+  echo;
+  echo;
+  echo +8G;
+
+  echo n; #новый раздел (остальное место под mnt(основной раздел))
+  echo;
+  echo;
+  echo;
+  
+  echo t;
+  echo 1;
+  echo 4;
+  
+  echo t;
+  echo 2;
+  echo 19;
+
+  echo w;
+) | fdisk --wipe always /dev/sda
+
+
 mkfs.ext2 /dev/sda1   &&
 mkswap /dev/sda2      &&
 mkfs.ext4 /dev/sda3   &&
 swapon /dev/sda2      &&
 mount /dev/sda3 /mnt  &&
+
+pacman-key --refresh-keys
 reflector --latest 10 --sort rate --save /etc/pacman.d/mirrorlist   &&
 pacstrap /mnt base base-devel linux linux-firmware dhcpcd vim grub sudo   &&
 
